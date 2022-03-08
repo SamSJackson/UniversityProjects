60b6c42bef896c48ae990be4a46169fd2cc755d6
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-03-08 14:04:52 +0000

    AE2 - Starting file

---
+++ SecondYear/ADS2/AE2/AssessedADT/src/ArrayQueue.java
@@ -0,0 +1,19 @@
+
+
+// Note that this array queue is a min priority queue.
+// Also a binary heap array
+public class ArrayQueue {
+	
+	// Operations to contain
+	// Insert (Add an element into q)
+	// Min (returns the element with the smallest key)
+	// Extract-min (Removes and return the element of queue with smallest key)
+
+	// Heap array is created as 0 with root
+	// Each consequent tree is indexed from left to right
+	
+	// Going to create heap array with max-heap otherwise hard to remove from root with extract-min
+	
+	
+	
+}
---
+++ SecondYear/ADS2/AE2/AssessedADT/src/BinaryTreeQueue.java
@@ -0,0 +1,6 @@
+
+
+// Note that this queue is min-priority queue
+public class BinaryTreeQueue {
+
+}
